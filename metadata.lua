47f3113f42340368e669f9fab8a5f1f04a49b985
special_snowcat <df.mars@erinye.com>
2018-03-24 11:20:13 +0100

    sync with workshop

--- metadata.lua
+++ metadata.lua
@@ -1,15 +1,15 @@
 return PlaceObj('ModDef', {
 	'title', "Lua Console",
-	'description', "Adds a Lua console to the game that lets you run lua code.\r\n\r\nFeatures:\r\n- NEW watch expressions (updated every second)\r\n- NEW scratch pad, a space for your notes, snippets, and helper functions\r\n- Reload mods via menu\r\n- Minimizable and scales with UI on high resolution displays",
+	'description', "Adds a Lua console to the game that lets you run lua code.\r\n\r\nFeatures:\r\n- watch expressions (updated every second)\r\n- scratch pad, a space for your notes, snippets, and helper functions\r\n- Reload mods via menu\r\n- Minimizable and scales with UI on high resolution displays\r\n\r\nGithub page: https://github.com/thatfool/SMars-Lua-Console",
 	'tags', "lua, console",
 	'image', "luaConsolePreview.png",
 	'id', "fkm1iEz",
 	'steam_id', "1335979800",
 	'author', "special_snowcat",
-	'version', 8,
-	'lua_revision', 228003,
+	'version', 10,
+	'lua_revision', 228184,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1521724926,
+	'saved', 1521886775,
 })
